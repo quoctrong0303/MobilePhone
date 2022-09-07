6f5b60f05861b2b9f8a6c21a215640b358edccb5
quoctrong0303 <quoctrong0303@gmail.com>
2022-09-07 21:07:50 +0700

    edit comit

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,48 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+var scrollButton = $(".arrow-scroll");
+var body = document.body,
+  html = document.documentElement;
 
-
-
+var height = Math.max(
+  body.scrollHeight,
+  body.offsetHeight,
+  html.clientHeight,
+  html.scrollHeight,
+  html.offsetHeight
+);
 isToggle = false;
 function toggleList() {
-    var lists =  $$('.toggle-list');
-    lists.forEach(element => {
-        element.onclick = () => {
-            isToggle = !isToggle;
-            if (isToggle) {
-                element.classList.add('show');
-                element.classList.remove('hide');
-
-            } else {
-                element.classList.remove('show');
-                element.classList.add('hide');
-            }
-
-        }
-    });
+  var lists = $$(".toggle-list");
+  lists.forEach((element) => {
+    element.onclick = () => {
+      isToggle = !isToggle;
+      if (isToggle) {
+        element.classList.add("show");
+        element.classList.remove("hide");
+      } else {
+        element.classList.remove("show");
+        element.classList.add("hide");
+      }
+    };
+  });
 }
 toggleList();
+
+window.onscroll = () => {
+  if (
+    document.body.scrollTop > height / 4 ||
+    document.documentElement.scrollTop > height / 4
+  ) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+};
+
+scrollButton.onclick = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+
+console.log(height / 2);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   important: true,
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        'primary-color': "#FFD333",
-        'secondary-color': "#",
-        'text-color': "#",
-        'background-color': "#",
-        'border-color': '',
+        "primary-color": "#FFD333",
+        "secondary-color": "#",
+        "text-color": "#",
+        "background-color": "#",
+        "border-color": "",
       },
       screens: {
-        'mobile': {'max': '739px'},
+        mobile: { max: "739px" },
         // => @media (max-width: 739px) { ... }
-  
-        'tablet': {'min': '740px', 'max': '1023px'},
+
+        tablet: { min: "740px", max: "1023px" },
         // => @media (min-width: 740px and max-width: 1023px) { ... }
-  
-        'pc': {'min': '1024px'},
+
+        pc: { min: "1024px" },
         // => @media (min-width: 1024px) { ... }
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animations: {},
     },
   },
   plugins: [
-    require('@tailwindcss/line-clamp'),
+    require("@tailwindcss/line-clamp"),
     // ...
   ],
-}
+};
